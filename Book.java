603b214eceec256b1bb3cdd47e77fa542762d808
arnieS06 <sharma.arnav2016@gmail.com>
2025-04-07 22:34:28 -0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,59 @@
+public class Book {
+    private String title;
+    private String author;
+    private boolean isCheckedOut = false;
+    private String borrower = null;
+    private LocalDate dueDate = null;
+
+    public Book(String title, String author) {
+        try {
+            this.title = title;
+            this.author = author;
+        } catch (IllegalArgumentException e) {
+            System.out.println("ERROR: your mistake was " + e.getMessage());
+        }
+
+    }
+
+    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean isCheckedOut) {
+        this.isCheckedOut = isCheckedOut;
+    }
+
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
