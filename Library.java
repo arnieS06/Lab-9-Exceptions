e9c36924621bdd8f30a6e8e5f275055b06ad931e
arnieS06 <sharma.arnav2016@gmail.com>
2025-04-17 12:46:46 -0700

    Update Library.java

--- Library.java
+++ Library.java
@@ -3,8 +3,50 @@ public class Library {
     private int bookCount;
     private final int MAX_BOOKS = 1000;
 
+    public int getBooksIndex() {
+        for (int n = 0; n < books.length; n++) {
+            if (books[n] == null) {
+                return n;
+            }
+        }
+        return MAX_BOOKS - 1;
+    }
+
+    public boolean containsBook(Book b) {
+        for (int n = 0; n < this.books.length; n++) {
+            if (this.books[n].getAuthor().equals(b.getAuthor()) && this.books[n].getTitle().equals(b.getTitle())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public Library() {
         this.books = new Book[MAX_BOOKS];
+    }
+
+    public void addBook(Book b) {
+        try {
+            if (this.books.length > MAX_BOOKS) {
+                throw new IllegalStateException();
+            }
+
+            else if (containsBook(b)) {
+                throw new IllegalStateException();
+            }
+
+            else if (this.books[0] == null) {
+                this.books[0] = b;
+
+            }
+
+            books[getBooksIndex()] = b;
+
+            this.bookCount++;
+
+        } catch (IllegalStateException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 
