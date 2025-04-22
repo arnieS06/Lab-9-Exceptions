a128146ac302ed107810f2f782e50bb7df6bfa24
arnieS06 <sharma.arnav2016@gmail.com>
2025-04-21 22:37:14 -0700

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,7 @@
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("This class was made in order to create a valid artifact for this lab. Ignore.");
+
+    }
+}
